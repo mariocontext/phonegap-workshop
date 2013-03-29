270d4c17e35ca93b6e547bbbe27af2e8af04ea46
Mario Noble <mario@contextmultimedia.com>
2013-03-28 21:05:41 -0700

    temp commit on registerEvent

--- js/main.js
+++ js/main.js
@@ -24,10 +24,11 @@ var app = {
 
     initialize: function() {
         var self = this;
+        this.registerEvents();
         this.store = new MemoryStore(function() {
              $('body').html(new HomeView(self.store).render().el);
         });
-        this.registerEvents();
+        
     }
 
 };
