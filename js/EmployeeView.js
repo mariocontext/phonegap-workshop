fc26c0a446d24de1faada551a5b47a6be897361c
Mario Noble <mario@contextmultimedia.com>
2013-03-29 13:14:18 -0700

    added contacts feature with app call prepended try with stepped code

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -1,5 +1,5 @@
 var EmployeeView = function(employee) {
- 
+
     this.initialize = function() {
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
@@ -7,24 +7,24 @@ var EmployeeView = function(employee) {
     };
 
     this.render = function() {
-	    this.el.html(EmployeeView.template(employee));
-	    return this;
-	};
+        this.el.html(EmployeeView.template(employee));
+        return this;
+    };
 
-	this.addLocation = function(event) {
-	    event.preventDefault();
-	    console.log('addLocation');
-	    navigator.geolocation.getCurrentPosition(
-	        function(position) {
-	            $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
-	        },
-	        function() {
-	            alert('Error getting location');
-	        });
-	    return false;
-	};
+    this.addLocation = function(event) {
+        event.preventDefault();
+        console.log('addLocation');
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                $('.location', this.el).html(position.coords.latitude + ',' +position.coords.longitude);
+            },
+            function() {
+                alert('Error getting location');
+            });
+        return false;
+    };
 
-	this.addToContacts = function(event) {
+    this.addToContacts = function(event) {
         event.preventDefault();
         console.log('addToContacts');
         if (!navigator.contacts) {
@@ -32,17 +32,17 @@ var EmployeeView = function(employee) {
             return;
         }
         var contact = navigator.contacts.create();
-        contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+        contact.name = {givenName: app.currentEmployee.firstName, familyName:  app.currentEmployee.lastName};
         var phoneNumbers = [];
-        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
-        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+        phoneNumbers[0] = new ContactField('work', app.currentEmployee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', app.currentEmployee.cellPhone, true); // preferred number
         contact.phoneNumbers = phoneNumbers;
         contact.save();
         return false;
     };
- 
+
     this.initialize();
- 
- }
- 
+
+}
+
 EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
