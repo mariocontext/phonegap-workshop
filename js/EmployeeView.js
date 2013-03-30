677bf8d544d3f533dfd3f8e09a43c47c5c97eb82
Mario Noble <mario@contextmultimedia.com>
2013-03-29 18:15:24 -0700

    minor view edit

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -33,10 +33,10 @@ var EmployeeView = function(employee) {
             return;
         }
         var contact = navigator.contacts.create();
-        contact.name = {givenName: app.currentEmployee.firstName, familyName:  app.currentEmployee.lastName};
+        contact.name = {givenName: employee.firstName, familyName:  employee.lastName};
         var phoneNumbers = [];
-        phoneNumbers[0] = new ContactField('work', app.currentEmployee.officePhone, false);
-        phoneNumbers[1] = new ContactField('mobile', app.currentEmployee.cellPhone, true); // preferred number
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
         contact.phoneNumbers = phoneNumbers;
         contact.save();
         return false;
